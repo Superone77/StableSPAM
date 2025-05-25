7b2ee3a8ac65feeeea0dc440bde0790dc3e2996a
One7 <yangwq177@163.com>
2025-05-25 17:46:42 +0800

    updtae hadamard_transform implement from fast_hadamard_transform library

--- utils/hadamard_transformer_helper.py
+++ utils/hadamard_transformer_helper.py
@@ -1,5 +1,23 @@
 import torch
-from hadamard_transform import hadamard_transform
+from scipy.linalg import hadamard
+import math
+import torch.nn.functional as F
 
+## Ref from fast_hadamard_transform
 def naive_hadamard_transform_with_scale(x, scale=1.0):
-    return hadamard_transform(x) * scale 
+    """
+    x: (..., dim)
+    out: (..., dim)
+    """
+    if hadamard is None:
+        raise ImportError("Please install scipy")
+    x_shape = x.shape
+    dim = x.shape[-1]
+    x = x.reshape(-1, dim)
+    log_dim = math.ceil(math.log2(dim))
+    dim_padded = 2 ** log_dim
+    if dim != dim_padded:
+        x = F.pad(x, (0, dim_padded - dim))
+    out = F.linear(x, torch.tensor(hadamard(dim_padded, dtype=float), dtype=x.dtype, device=x.device))
+    out = out * scale
+    return out[..., :dim].reshape(*x_shape)
